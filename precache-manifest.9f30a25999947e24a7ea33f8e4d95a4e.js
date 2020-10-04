9f32bccce381b1f9a21e2c98867547c5717a12c7
Aalyousif <aalyousif@deakin.edu.au>
2020-10-05 07:24:24 +1100

    build

---
+++ precache-manifest.9f30a25999947e24a7ea33f8e4d95a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a9e7b6c45c6888cf2d9268a465d1a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d27c16b8d2ab0b05dcea",
+    "url": "/static/css/main.b3c4e3d2.chunk.css"
+  },
+  {
+    "revision": "447e97d714cf08d2dc26",
+    "url": "/static/js/2.b15b830c.chunk.js"
+  },
+  {
+    "revision": "d27c16b8d2ab0b05dcea",
+    "url": "/static/js/main.a420381d.chunk.js"
+  },
+  {
+    "revision": "723736c6827838f916b4",
+    "url": "/static/js/runtime-main.28f14444.js"
+  },
+  {
+    "revision": "924ab24f2cf5d8c5db84b5dd8cfceb9c",
+    "url": "/static/media/profile_1.924ab24f.jpg"
+  }
+]);
